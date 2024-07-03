6566e437788b9addb25c73ed3147f88da11f9aae
radityafs <radit@student.uns.ac.id>
2024-07-03 17:37:16 +0700

    Remove unnecessary files and directories

--- routes/cashier.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\Cashier\DashboardController;
-use Illuminate\Support\Facades\Route;
-
-Route::group(['prefix'=>'cashier', 'as'=>'cashier.', 'middleware' => ['auth', 'role:cashier']], function(){
-    Route::get('dashboard', [DashboardController::class,'index'])->name('dashboard');
-});

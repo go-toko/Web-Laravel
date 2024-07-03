76189bb99ce27dd86b58eae8140b842949be45ea
radityafs <radit@student.uns.ac.id>
2024-07-03 17:38:38 +0700

    Remove unnecessary files and directories

--- config/midtrans.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'client_key' => env('MIDTRANS_CLIENT_KEY'),
-    'server_key' => env('MIDTRANS_SERVER_KEY'),
-    'merchant_id' => env('MIDTRANS_MERCHANT_ID'),
-    'production' => false
-];
